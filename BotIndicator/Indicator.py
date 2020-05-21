694161a88d9b4960d2bfa48d909bf644ea0972a9
Drako06 <dario_acs12@hotmail.com>
2020-05-21 12:08:44 -0500

    lee los mercados y añade las posibles señales con threads

--- BotIndicator/Indicator.py
+++ BotIndicator/Indicator.py
@@ -1,36 +1,37 @@
 import time
-
-from binance.client import Client
-#import math
-import random
+import sqlite3
+import logging
+import datetime
+import threading
+import pandas as pd
 from ta import momentum
 from ta import volatility
-from BotIndicator.Utility import client, bd
-#from BotIndicator.CheckTable import validTable
-#from BotIndicator import comunication_binance
-import pandas as pd
-import sqlite3
-#from RFunctions.CheckTable import validTable
+from concurrent.futures import ThreadPoolExecutor
+from binance.client import Client
+from BotIndicator.Utility import client, conn
 
-#from RFunctions.comunication_binance import wsocket
+logging.basicConfig(level=logging.DEBUG, format='[%(levelname)s] (%(threadName)-s %(message)s')
 
+def markets(bases):
+    pass
 
+def signal():
+    pass
 
 def marketChoice(interval):
-
-    products = client.get_products()
-    #market = 'MATICUSDT'
-
-    data = products['data']
+    products = client.get_exchange_info()
+    #print(products['symbols'])
+    data = products['symbols']
     criptos = []
     for elemento in data:
         #print(elemento['symbol'], elemento['market'])
-        if elemento['market'] == 'USDT':
+        if elemento['quoteAsset'] == 'USDT':
             criptos.append(elemento['symbol'])
     #print(criptos)
     print("Elejimos las USDT")
 
     ascendientes = []
+    moneda = []
     # tickers = client.get_ticker(symbol='OGNUSDT')
     for i in criptos:
         try:
@@ -39,113 +40,131 @@ def marketChoice(interval):
             #print(tickers)
             #print(float(tickers['quoteVolume']))
             if float(tickers['quoteVolume']) > 700000:  #float(tickers['priceChangePercent']) >= 0.3 and
-                ascendientes.append([tickers['symbol'], tickers['priceChangePercent']])
+                ascendientes.append(tickers['symbol'])
+                moneda.append(i)
         except:
             pass
     #print(len(ascendientes))
     print("Tenemos los Ascendientes")
+    return ascendientes
     #print(ascendientes)
 
-    """
-    ascendientes = ['VETUSDT', 'MATICUSDT', 'XLMUSDT', 'BULLUSDT'] #, 'TRXUSDT', 'IOSTUSDT'
-                    #'EOSUSDT']  # , 'ETCUSDT', 'BNBUSDT', 'XLMUSDT', 'MATICUSDT']
-    """
-    markets = []
+def indice(symbol, interval):
+    #markets = []
+    #print(ascendientes)
+    #for j in ascendientes:
+    logging.debug("Thread con la criptomoneda de: " + symbol)
+    dataset = client.get_klines(symbol=symbol, interval=interval, limit=21)
+    data = pd.DataFrame(dataset, dtype=float)
+    data.pop(1)
+    data = data.iloc[:, :5]
+    data = data.rename(
+        columns={0: "FechaApertura", 2: 'ValorMaximo', 3: 'ValorMinimo', 4: 'Cierre', 5: 'Volumen'})
+    columns = ['FechaApertura', 'ValorMaximo', 'ValorMinimo', 'Cierre', 'Volumen']
+    data = data[columns]
+    data = pd.DataFrame(data, dtype=float)
+    #print(data)
+    valorMaximo = data['ValorMaximo']
+    valorMinimo = data['ValorMinimo']
+    cierre = data['Cierre']
+    volumen = data['Volumen']
+        #fecha = data['FechaApertura']
+    a = len(data.index) - 1
+    #print(a)
+    rsi = momentum.rsi(cierre, n=14, fillna=False)
+    #print("El RSI: " + str(rsi))
+    mfi = momentum.money_flow_index(valorMaximo, valorMinimo, cierre, volumen, n=14, fillna=True)
+    #print("El MFI: " + str(mfi))
+    boll_high = volatility.bollinger_hband(cierre, n=20, ndev=2, fillna=True)
+    #print("El BOLLINGER: " + str(boll_high))
+        # boll_low = volatility.bollinger_lband(cierre, n=20, ndev=2, fillna=True)
+    #print(cierre[a], boll_high[a])
+    ganancia = abs(((cierre[a] * 100) / boll_high[a]) - 100)
+    print(ganancia)
 
-    #ascendientes = ['VETUSDT', 'MATICUSDT', 'BULLUSDT', 'BEARUSDT', 'XLMUSDT', 'TRXUSDT', 'ICXUSDT']
-    for j, k in ascendientes:
-        dataset = client.get_klines(symbol=j, interval=interval, limit=20)
-        data = pd.DataFrame(dataset, dtype=float)
-        data.pop(1)
-        data = data.iloc[:, :5]
-        data = data.rename(
-            columns={0: "FechaApertura", 2: 'ValorMaximo', 3: 'ValorMinimo', 4: 'Cierre', 5: 'Volumen'})
-        columns = ['FechaApertura', 'ValorMaximo', 'ValorMinimo', 'Cierre', 'Volumen']
-        data = data[columns]
-        #a = len(data.index)
+    print(symbol, rsi[a - 1], rsi[a], ganancia)
+    print(symbol, mfi[a - 1], mfi[a], ganancia)
 
-        #row = [fecha, maximo, minimo, cierre, volumen]
-        # print(data.loc[len(data.index)])
-        #data.loc[len(data.index)] = row
-        data = pd.DataFrame(data, dtype=float)
-        valorMaximo = data['ValorMaximo']
-        valorMinimo = data['ValorMinimo']
-        cierre = data['Cierre']
-        volumen = data['Volumen']
-        fecha = data['FechaApertura']
-        a = len(data.index) - 1
-        rsi = momentum.rsi(cierre, n=14, fillna=False)
-        mfi = momentum.money_flow_index(valorMaximo, valorMinimo, cierre, volumen, n=14, fillna=True)
-        boll_high = volatility.bollinger_hband(cierre, n=20, ndev=2, fillna=True)
-        #boll_low = volatility.bollinger_lband(cierre, n=20, ndev=2, fillna=True)
-        ganancia = abs(((cierre[a] * 100) / boll_high[a]) - 100)
+    #if mercados(mfi, ganancia, a, rsi):
+    #     markets.append(symbol)
 
-        print(j, rsi[a-1], rsi[a], ganancia)
-        print(j, mfi[a-1], mfi[a], ganancia)
+    #print(markets)
+    if mercados(mfi, ganancia, a, rsi):
+        #    market = random.choice(markets)
 
-        if 28 >= mfi[a] > mfi[a - 1] and ganancia > 0.5 and rsi[a-1] < rsi[a] < 40:
-            markets.append(j)
+        #    print("Imprime el mercado elegido: " + market)
 
-    print(markets)
-    if len(markets) != 0:
-        market = random.choice(markets)
+        #### VALIDAMOS LA TABLA PARA LA MONEDA SELECCIONADA
 
-        print("Imprime el mercado elegido: " + market)
-
-    #### VALIDAMOS LA TABLA PARA LA MONEDA SELECCIONADA
-
-        return market
+        return symbol
     else:
-        print("No se han encontrado Mercados disponibles")
+        print("Mercado No Apropiado")
         return False
-    #validTable(market)
 
+def mercados(mfi, ganancia, a, rsi):
+    if 28 >= mfi[a] > mfi[a - 1] and ganancia > 0.5 and rsi[a - 1] < rsi[a] < 40:
+        return True
 
-    #### CREAMOS UNA INSTANCIA DE LA CLASE DE COMUNICACION CON EL WEBSOCKET
-
-
-    """se hardcodea el mercado para volver a una tabla donde se quiere seguir"""
-
-
-    #market = ['VETUSDT', 'MATICUSDT', 'XLMUSDT', 'BULLUSDT', 'BEARUSDT']
-    #market = 'VETUSDT'
-    #market = random.choice(market)
-    print("Imprime el mercado elegido: " + market)
-    #market = 'XLMUSDT'
-    return market
-
-    #return tableName
-
-
+def signals(rsi, ema, sma, mfi, a, cierre):
+    if 50 <= rsi[a] > rsi[a - 1] and rsi[a - 1] < 70 and ema[a] > sma[2] \
+            and ema[a - 2] < sma[0] and mfi[a] > mfi[a - 1] and cierre[a - 1] > sma[1]:
+        return True
 
 
 def main():
+    bd = sqlite3.connect(conn.pathDB)
     cursor = bd.cursor()
-
+    base = 'USDT'
+    intervalo = Client.KLINE_INTERVAL_5MINUTE
 
     while True:
-        cursor.execute("SELECT MAX(idTrade) FROM TB_TradesTest")  # + self.market)
-        indice = cursor.fetchone()[0]
-        print("El maximo Indice es de: " + str(indice))
-        cursor.execute("SELECT BanderaCompra FROM TB_TradesTest  Where idTrade = ?", (indice,))
-        # + self.market + " Where idTrade = ?", (self.indice, ))
-        bandera_compra = cursor.fetchone()[0]
-        print("El status de bandera compra es: " + str(bandera_compra))
-        print("Iniciamos el intervalo dentro del main")
-        intervalo = Client.KLINE_INTERVAL_5MINUTE
-        while True:
-            if bandera_compra == 1:
-                cursor.execute("SELECT Mercado FROM TB_TradesTest Where idTrade = ? ", (indice,))
-                market = cursor.fetchone()[0]
-                print("El mercado seleccionado es: " + market)
-            if bandera_compra != 1:
-                print("Elegimos el mercado dentro del main")
-                market = marketChoice(intervalo)
-                time.sleep(2)
-            if market is not False:
-                print("EL mercado no es falso")
-                break
-            #validTable(market)
+
+        ascendientes = marketChoice(intervalo)
+        #indice(ascendientes, intervalo)
+        print("Imprime los ascendientes")
+        print(ascendientes)
+        #s = threading.Semaphore(10)
+        threads = list()
+        exeutor = ThreadPoolExecutor(max_workers=10)
+        for i in ascendientes:
+            thr = exeutor.submit(indice, i, intervalo)
+            #thr = threading.Thread(target=indice, args=(i, intervalo))
+            threads.append(thr)
+            time.sleep(0.1)
+            #thr.start()
+        results = list()
+        for i in threads:
+            print("Imprime el valor de thread: " + str(i.result()))
+            #print(i)
+            #th = i.join()
+            if i.result():
+                results.append(i.result())
+
+        print(results)
+        if len(results) != 0:
+            break
+
+    #    for i in markets:
+    ##        market = i.replace(base, "")
+     #       cursor.execute("SELECT Status FROM TB_Signals Where Cripto = ?", market)
+     #       if 0 == cursor.fetchone() is None:
+     #           cursor.execute("INSERT INTO TB_Signal(Cripto, Base, fechaSeñal, Status) VALUES(?,?,?,?)", market, base, datetime.datetime.now(), 1)
+     #           bd.commit()
+
+
+      #  while True:
+      #      #if bandera_compra == 1:
+      #      #    cursor.execute("SELECT Mercado FROM TB_TradesTest Where idTrade = ? ", (indice,))
+      #      #    market = cursor.fetchone()[0]
+            #    print("El mercado seleccionado es: " + market)
+            #if bandera_compra != 1:
+    print("Elegimos el mercado dentro del main")
+     #       market = marketChoice(intervalo)
+      #      time.sleep(2)
+       #     if market is not False:
+        #        print("EL mercado no es falso")
+         #       break
+         #   #validTable(market)
         #print("Comunicacion con binance websocket")
         #socket = comunication_binance.Socket(market, intervalo)
         #print("Ejecutamos la funcion wsocket()")
